c9a3c4155f10b7c0856107423701de1b87b45f14
Gustav Uwe <90202107+gustavuwe@users.noreply.github.com>
2022-03-29 08:50:58 -0300

    Placing additional descriptions

--- GformBot.py
+++ GformBot.py
@@ -6,9 +6,9 @@ import pyperclip
 
 navegador = webdriver.Chrome(executable_path=r'./chromedriver.exe')
 
-navegador.get('https://docs.google.com/forms/d/e/1FAIpQLSdUwxgtiddg9IS9D7OPAgqDm6TdY0CHx6MW7292p7a5ZDhkXg/viewform')
+navegador.get('link here')
 time.sleep(5)
-emailadress = "luislima@gmail.com"
+emailadress = "email here" #if dont need email you can delete that variable and the variable "email"
 i = 0
 while i <= 10000:
 
@@ -18,8 +18,8 @@ while i <= 10000:
     pyperclip.copy(emailadress)
     pyautogui.hotkey('ctrl', 'v')
 
-    opcao = navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div/span/div/div[3]/label')
-    opcao.click()
+    option = navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div/span/div/div[3]/label')
+    option.click()
 
     submit = navegador.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div/div[1]/div/span')
     submit.click()
@@ -27,7 +27,7 @@ while i <= 10000:
     restart = navegador.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
     restart.click()
 
-    print(i,' votos contabilizados')
+    print(i,' counted votes')
     i += 1
 
     if i > 10000:
